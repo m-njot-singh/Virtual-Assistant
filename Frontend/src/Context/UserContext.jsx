380d592c60494e66f697e3aed19803199df92720
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2025-06-29 23:58:08 +0530

    Update UserContext.jsx
    add backend server Url Live

--- Frontend/src/Context/UserContext.jsx
+++ Frontend/src/Context/UserContext.jsx
@@ -4,7 +4,7 @@ import axios from "axios"
 import { useEffect } from "react";
 export const UserDataContext= createContext()
 function UserContext({children}){
-    const serverUrl = "http://localhost:8000"
+    const serverUrl = "https://virtual-assistant-7tkp.onrender.com"
     const [userData,setUserData] = useState(null)
     const [frontendImage, setFrontendImage] = useState(null)
     const [backendImage, setBackendImage] = useState(null)
